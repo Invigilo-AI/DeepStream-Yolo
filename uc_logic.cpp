6463840c676c0711509897c342c29e527813511f
dioptrique <h0910139@gmail.com>
2024-02-13 02:54:39 +0000

    uc_logic updated

--- uc_logic.cpp
+++ uc_logic.cpp
@@ -7,6 +7,7 @@
 #include <curl/curl.h>
 #include <string>
 #include <cstring>
+#include <cmath>
 // #include <thread>
 
 using namespace std;
@@ -14,6 +15,7 @@ using namespace std;
 static std::unordered_map<int, unordered_map<uint64_t, int64_t>> ds; // stream_id->tracker_id->last sent alert
 
 const char* final_access_token = "";
+static bool flag = false;
 
 size_t WriteCallback(void *contents, size_t size, size_t nmemb, std::string *response) {
         response->append((char *)contents, size * nmemb);
@@ -55,14 +57,22 @@ const char* getauthbear(const std::string& app_user, const std::string& app_pwd)
     return access_token;
 }
 
-
+static int64_t time_diff = 0;
 extern "C" {
     bool check_alert(int stream_id, uint64_t track_id, int64_t current_time) {
+        
+        if(flag==false && current_time>1){
+            int64_t n_digits = floor(log10(current_time)); 
+            int64_t power_of_10 = pow(10, n_digits);
+            time_diff = current_time + power_of_10;
+            flag=true;
+        }
         auto it = ds.find(stream_id);
         if (it != ds.end()) {
             auto it2 = ds[stream_id].find(track_id);
             if (it2 != ds[stream_id].end()) {
-                if (current_time - ds[stream_id][track_id] > 900000000000)  {
+                if (current_time - ds[stream_id][track_id] > time_diff)  {
+                    // std::cout<<current_time<<"-"<<ds[stream_id][track_id]<<"="<<current_time - ds[stream_id][track_id]<<std::endl;
                     return true;
                 } else {
                     return false;
@@ -99,3 +109,52 @@ extern "C" {
 
 
 
+
+#include <opencv2/opencv.hpp>
+#include <fstream>
+#include <sstream>
+
+// using namespace cv;
+using namespace std;
+
+void bbox_cv(){
+    // Read the image
+    cv::Mat image = cv::imread("/opt/nvidia/deepstream/deepstream-6.2/DeepStream-Yolo/download_frames/OSD_frame_8_0.jpg");
+    if (image.empty()) {
+        cerr << "Error: Could not read the image." << endl;
+        return;
+    }
+
+    // Read annotation file
+    ifstream annotationFile("/opt/nvidia/deepstream/deepstream-6.2/DeepStream-Yolo/detections/00_000_000008.txt");
+    if (!annotationFile.is_open()) {
+        cerr << "Error: Could not open the annotation file." << endl;
+        return;
+    }
+
+    string line;
+    while (std::getline(annotationFile, line)) {
+        // Parse annotation
+        std::istringstream iss(line);
+        std::string className;
+        iss >> className;
+
+        std::vector<float> bbox;
+        float coord;
+        while (iss >> coord) {
+            bbox.push_back(coord);
+        }
+
+
+        cv::rectangle(image, cv::Point(bbox[1], bbox[2]), cv::Point(bbox[5], bbox[6]), cv::Scalar(0, 255, 0), 2);
+
+        cv::putText(image, className, cv::Point(bbox[1], bbox[2] - 10), cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 255, 0), 2);
+    }
+
+    // Display and save the result
+    cv::imshow("Image with Bounding Boxes", image);
+    cv::imwrite("/opt/nvidia/deepstream/deepstream-6.2/DeepStream-Yolo/annotated_image.jpg", image);
+    cv::waitKey(0);
+
+    return;
+}
